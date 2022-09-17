41a6c4f5062cfbb04a5aad3c47f11b77167cc4f0
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-09-17 17:50:51 -0400

    added routes for register page

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,9 @@ Rails.application.routes.draw do
     
   end
 
+  get '/register' => 'users#new'
+  post '/register' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
