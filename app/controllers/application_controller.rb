846b6dec3f4d8f044445e6810293c6389cf1a7c4
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-09-17 18:42:16 -0400

    updated application controller for user and added  login/logout/register navbar options with logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,12 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
+
+  helper_method :current_user
 end
