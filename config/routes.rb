b8a1f153e612f0de385ca88eb9388908d751182b
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-09-17 18:02:36 -0400

    renamed routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,8 @@ Rails.application.routes.draw do
     
   end
 
-  get '/register' => 'users#new'
-  post '/register' => 'users#create'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
