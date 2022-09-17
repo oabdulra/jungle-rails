8338c324a90a24132541b6a3dde0866dfdca2fe3
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-09-17 15:41:05 -0400

    fixed some bugs

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USER'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
-
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['PASSWORD']
+  
   def index
     @categories = Category.order(id: :desc).all
   end
@@ -14,12 +14,13 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :categories], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created'
     else
       render :new
     end
   end
 
+ 
 
   private
 
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,11 +1,11 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['PASSWORD']
+
   def index
     @products = Product.order(id: :desc).all
   end
 
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['PASSWORD']
-  
   def new
     @product = Product.new
   end
@@ -14,7 +14,7 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+      redirect_to [:admin, :products], notice: 'Product created'
     else
       render :new
     end
@@ -23,7 +23,7 @@ class Admin::ProductsController < ApplicationController
   def destroy
     @product = Product.find params[:id]
     @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
+    redirect_to [:admin, :products], notice: 'Product deleted'
   end
 
   private
