d62d443bba3b7d05aaa5a9af1fb878794254947f
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-09-17 23:21:14 -0400

    user model validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,19 @@ RSpec.describe User, type: :model do
 
   describe 'Validation' do
 
+    it 'validates that a new user is registered if all required fields are filled in' do
+      @user = User.new(name: 'Osama', email: 'osama@osama.com', password: 'osama')
+      @user.save
+      expect(@user).to be_valid
+      end
+
+      it 'validates that a new user enters a name' do
+        @user = User.new(name: '', email: 'bob@bob.com', password: 'bobby')
+        @user.save
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+        end
+  
+    
 
 
 
